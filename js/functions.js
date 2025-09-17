f61e462039e354da3c5560178bfd6663f783ffa5
dennyhappysomuch <den_happy@bk.ru>
2025-09-17 14:00:41 +0700

    Добавит файл в исключение eslint-disable no-unused-vars

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 const isLengthValid = (str, maxLength) => str.length <= maxLength;
+
 
 const isPalindrome = (str) => {
   str = str.toLowerCase().replace(/\s|[.,;!?:"'-]/g, '');
 
   return str === str.split('').reverse().join('');
 };
+
